00c9bb8aaf127bc9dad1b44a5e98f6ccee1a4730
Probably Nate <159125401+Nebula434@users.noreply.github.com>
2026-03-20 03:03:17 -0700

    pygame modules, debugging scoring.
    Adjusting scoring, if code is ran as is, it would be shown how it can't score currently.
    
    Major Bug:
    No matter what card is drawn, the score remains a flat 10 with some variability to debug2 from the number 10.

--- game_file.py
+++ game_file.py
@@ -1,9 +1,10 @@
 import random
-#import pygame 
+import pygame 
+import button
 
 #pygame variables
 #pygame.init()
-#pscreen = pygame.display.set_mode((1280,720))
+#screen = pygame.display.set_mode((1024,768))
 #clock = pygame.time.Clock()
 #running = True
 #
@@ -26,6 +27,10 @@ Player_Lost = False
 
 #image loading
 
+#start_img = pygame.image.load('assests/menu/start_button.png').convert_alpha()
+#quit_img = pygame.image.load('assests/menu/quit_button.png').convert_alpha()
+#settings_img = pygame.image.load('assests/menu/settings_button.png').convert_alpha()
+#title_img = pygame.image.load('assests/menu/title_screen.png').convert_alpha()
 
 #temp_button = pygame.image.load('game_images/button.png')
 #
@@ -79,20 +84,19 @@ def draw_card(): #some type of variable to put card into dealer or player#
 			drawn_face = Face_cards[1]
 		if drawn_face == 3:
 			drawn_face = Face_cards[2]
-				 
 		drawn_face = str(drawn_face)
 		drawn_card = drawn_face + " of " + drawn_type
 		Player_Hand.append(drawn_card)		
 
 	if Prob > 0.23:
 		drawn_number = Card_numbers[round(random.uniform(1,9))]
+		if drawn_number == 1:
+			drawn_number = "Ace"
 		drawn_face = ""
 		drawn_number = str(drawn_number)
 		drawn_card = drawn_number + " of " + drawn_type
-		#drawn_card.append(drawn_number)
-		#drawn_card.append(drawn_type)
 		Player_Hand.append(drawn_card)
-	print(drawn_card)
+	#print(drawn_card)
 	return(drawn_card,drawn_number,drawn_face)
 
 def dealer_draw_card(): #TODO: CREATE DEALER#
@@ -124,119 +128,76 @@ def dealer_draw_card(): #TODO: CREATE DEALER#
 	print(drawn_card)	
 	return
 
-#TODO: CREATE DRAW SYSTEM, UTILIZE Player_Hand.append(draw_card())#
-player_hit = False
-#Drawing Tests
-def ScoreCard(): #Current bug only prints the value of 10 or 20 for some reason, never the actual value of the card.
-	for card in range(len(Player_Hand)):
-		current_card = Player_Hand[card]
+Player_Hand_Score = []
+Hand_scored = False
+def ScoreCard(times_needed): #Current bug only prints the value of 10 or 20 for some reason, never the actual value of the card.
+	for card in range(times_needed):
+		current_card = str(Player_Hand[card])
+		print(current_card)
 		#if statement hell.
-		if current_card == "Ace of Hearts":
-			cardscore = 11
-		if current_card == "1 of Hearts":
-			cardscore = 1
-		if current_card == "2 of Hearts":
-			cardscore = 2	
-		if current_card == "3 of Hearts":
-			cardscore = 3
-		if current_card == "4 of Hearts":
-			cardscore = 4
-		if current_card == "5 of Hearts":
-			cardscore = 5
-		if current_card == "6 of Hearts":
-			cardscore = 6
-		if current_card == "7 of Hearts":
-			cardscore = 7
-		if current_card == "8 of Hearts":
-			cardscore = 8
-		if current_card == "9 of Hearts":
-			cardscore = 9
-		if current_card == "10 of Hearts":
-			cardscore = 10
-		if current_card == "King of Hearts" or "Queen of Hearts" or "Jack of Hearts":
-			cardscore = 10
-		#All Hearts above this#
-		if current_card == "Ace of Diamonds":
-			cardscore = 11
-		if current_card == "1 of Diamonds":
-			cardscore = 1
-		if current_card == "2 of Diamonds":
-			cardscore = 2	
-		if current_card == "3 of Diamonds":
-			cardscore = 3
-		if current_card == "4 of Diamonds":
-			cardscore = 4
-		if current_card == "5 of Diamonds":
-			cardscore = 5
-		if current_card == "6 of Diamonds":
-			cardscore = 6
-		if current_card == "7 of Diamonds":
-			cardscore = 7
-		if current_card == "8 of Diamonds":
-			cardscore = 8
-		if current_card == "9 of Diamonds":
-			cardscore = 9
-		if current_card == "10 of Diamonds":
-			cardscore = 10
-		if current_card == "King of Diamonds" or "Queen of Diamonds" or "Jack of Diamonds":
-			cardscore = 10
-		#Diamonds above this#
-		if current_card == "Ace of Spades":
-			cardscore = 11
-		if current_card == "1 of Spades":
-			cardscore = 1
-		if current_card == "2 of Spades":
-			cardscore = 2	
-		if current_card == "3 of Spades":
-			cardscore = 3
-		if current_card == "4 of Spades":
-			cardscore = 4
-		if current_card == "5 of Spades":
-			cardscore = 5
-		if current_card == "6 of Spades":
-			cardscore = 6
-		if current_card == "7 of Spades":
-			cardscore = 7
-		if current_card == "8 of Spades":
-			cardscore = 8
-		if current_card == "9 of Spades":
-			cardscore = 9
-		if current_card == "10 of Spades":
-			cardscore = 10
-		if current_card == "King of Spades" or "Queen of Spades" or "Jack of Spades":
-			cardscore = 10
-		#Spades above this#
-		if current_card == "Ace of Clubs":
-			cardscore = 11
-		if current_card == "1 of Clubs":
-			cardscore = 1
-		if current_card == "2 of Clubs":
-			cardscore = 2	
-		if current_card == "3 of Clubs":
-			cardscore = 3
-		if current_card == "4 of Clubs":
-			cardscore = 4
-		if current_card == "5 of Clubs":
-			cardscore = 5
-		if current_card == "6 of Clubs":
-			cardscore = 6
-		if current_card == "7 of Clubs":
-			cardscore = 7
-		if current_card == "8 of Clubs":
-			cardscore = 8
-		if current_card == "9 of Clubs":
-			cardscore = 9
-		if current_card == "10 of Clubs":
-			cardscore = 10
-		if current_card == "King of Clubs" or "Queen of Clubs" or "Jack of Clubs":
-			cardscore = 10
-		#Clubs and thats it of if statement hell!#
-	total_card_scores = []
-	total_card_scores.append(cardscore)
-	print(total_card_scores)
+		if current_card[0:2] == 'Ace of Hearts' or 'Ace of Diamonds' or 'Ace of Clubs' or 'Ace of Spades':
+			cardscore = int(11)
+		if current_card[0] == '2':
+			cardscore = int(2)
+		if current_card[0] == '3':
+			cardscore = int(3)
+			
+		if current_card[0] == '4':
+			cardscore = int(4)
+			
+		if current_card[0] == '5':
+			cardscore = int(5)
+			
+		if current_card[0] == '6':
+			cardscore = int(6)
+			
+		if current_card[0] == '7':
+			cardscore = int(7)
+			
+		if current_card[0] == '8':
+			cardscore = int(8)
+			
+		if current_card[0] == '9':
+			cardscore = int(9)
+		if current_card[0] == '10':
+			cardscore = int(10)
+			print(current_card, "debug2")
+
+		if current_card[0] == "K" or "Q" or "J":
+			cardscore = float(10.0)
+			return cardscore
+		
+		print(cardscore)
+		cardscore =+ cardscore
+
+	return cardscore
 
 
-	#return(TotalHandScore)
+#Input Test for card#
+print("Start")
+draw_card()
+draw_card()
+print(Player_Hand)
+ScoreCard(int(len(Player_Hand)))
+player_input = str(input("Stand or Hit"))
+if player_input == "Hit" :
+	draw_card()
+	ScoreCard(int(len(Player_Hand)))
+	Hand_scored = True
+	print(Player_Hand_Score)
+if player_input == "Stand":
+	#dealer would draw here
+	ScoreCard(int(len(Player_Hand)))
+	print(Player_Hand_Score)
+
+
+
+
+
+
+
+
+
 
 #Player Hit System
 player_hit = False
@@ -247,29 +208,19 @@ def PlayerChoice(action):
 	if action == "Yes" or "Stand" or "Y":
 		player_hit = False
 		DealerTurn = True
-		ScoreCard()
+		return(player_hit)
 	if action == "No" or "Hit" or "N":
 		player_hit = True
 		draw_card()
 		print("Your Current Hand:\n", Player_Hand)
-		ScoreCard()
+		return(player_hit)
 
 
 
 
-#Input Tests not offical game#
-player_input = input("Draw a number of cards:")
-cards_wanted = int(player_input)
-for number in range(cards_wanted):
-	print("Here is your card")
-	draw_card()
-	print(Player_Hand)
-	print("I am drawing your next card")
 
 
-while len(Player_Hand) > 0 or DealerTurn == False or Player_Lost == False:
-	action_input = str(input("Would you like to stand or hit?"))
-	PlayerChoice(action_input)
+
 
 
 
@@ -278,15 +229,38 @@ while len(Player_Hand) > 0 or DealerTurn == False or Player_Lost == False:
 
 #Assigning Colors to the card
 
+#classes utilized for game below here
+class Menu():
+	def __init__(self,x,y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
 
+		self.image = pygame.transform.scale(image,(int(width * scale),int(height * scale)))
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x,y)
+	#def draw(self):
+		#screen.blit(self.image,(self.rect.x, self.rect.y))
+		
 
-
-
+#start_button = button.Button(483, 350, start_img,0.5)
+#exit_button = button.Button(483, 425, quit_img,0.5)
+#title_screen = Menu(0, -100, title_img, 2)
+#player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 #GAME LOOP#
 #while running: 
-#	pscreen.fill("purple")
-#	print(pygame.mouse.get_pos())
-#	#closes game#
+
+
+	#print(pygame.mouse.get_pos())
+#	screen.fill("white")
+#	title_screen.draw()	
+#	if start_button.draw(screen) == True:
+#		match_start = True
+#		print('Start')
+#	if exit_button.draw(screen) == True:
+#		print("exiting game")
+#		running = False
+	#closes game#
 #	for event in pygame.event.get():
 #		if event.type == pygame.QUIT:
 #			running = False
