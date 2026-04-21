2be3674383fc9a98dfc009565d1593409ddf04bd
Probably Nate <159125401+Nebula434@users.noreply.github.com>
2026-04-20 21:36:54 -0700

    Attempting to fix 3 loop crash

--- bin/logic.py
+++ bin/logic.py
@@ -1,52 +1,49 @@
 import random
 import card as deck
 
-#Variables for game
-Face_cards = ['King','Queen','Jack']
-Card_types = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
-Card_numbers = [1,2,3,4,5,6,7,8,9,10]
-FaceCard = False
-AceCard = False
+#Variables 
 Player_Hand = []
 Dealer_Hand = []
 Card = []
-HandValue = 0
-DealerHandValue = 0
 Prob = random.uniform(0,1)
-Player_Lost = False
 
 #Functions to be called throughout the game multiple times#
 # deck.draw_card((hand you are giving the card to), "Text you want to print out")
 # deck.ScoreCard((hand you are scoring aka Dealer_Hand or Player_Hand))
 #
+
+#Bug List#
+# After 3 iterations, the game loop ceases. Resulting in card's being drawn but not scored
+# and not returning to the game loop. Despite match_going & Player_Turn being true.
+
+
+
 #Input Test for card#
 match_going = True
 PlayerTurn = True
 total_hand_score = 0
 dealer_score = 0
-def reset():
-    Player_Hand = []
-    Dealer_Hand = []
+def reset(): #NOTE: Purpose of this function is to reset values that need to be used in the next loop
+    Player_Hand.clear()
+    Dealer_Hand.clear()
     total_hand_score = 0
     dealer_score = 0
     d_score = 0
-
+    player_score = 0
+    return Player_Hand, Dealer_Hand, total_hand_score, dealer_score, d_score, player_score
 def startMatch():
-    match_going == True
+    match_going = True
     deck.draw_card(Player_Hand,"Player")
     deck.draw_card(Player_Hand,"Player")
     deck.draw_card(Dealer_Hand,"Dealer")
-    player_score = deck.ScoreCard(Player_Hand)
-    d_score = deck.ScoreCard(Dealer_Hand)
     print("Current Dealer Hand \n", Dealer_Hand)
     print("Current Player Hand\n", Player_Hand)
-    return player_score, d_score
-
+    return (match_going)
 
 
 def endMatch():
-	match_going = False
-	return match_going
+    match_going = False
+    return match_going
 print("Start")
 startMatch()
 d_score = deck.ScoreCard(Dealer_Hand)
@@ -55,40 +52,40 @@ print("Dealer's Hand Score:\n", d_score)
 print("Player's Hand Score:\n", player_score)
 # The hopefully working game loop :) - This is awesome thank u Dustin :D
 while match_going and PlayerTurn:
-	# Grab input inside the loop so it asks every time
-	player_input = input("Stand or Hit:\n").lower()
+    # Grab input inside the loop so it asks every time
+    player_input = input("Stand or Hit:\n").lower()
 
-	if player_input == "hit":
-		deck.draw_card(Player_Hand,"Player")
-		player_score = deck.ScoreCard(Player_Hand)
-		print("Player's Hand Score Currently:\n", player_score)
+    if player_input == "hit":
+        deck.draw_card(Player_Hand,"Player")
+        player_score = deck.ScoreCard(Player_Hand)
+        print("Player's Hand Score Currently:\n", player_score)
 
-		# Check if they busted
-	if player_score > 21:
-			print("Past 21! You busted...")
-			PlayerTurn = False
-			match_going = False
+        # Check if they busted
+    if player_score > 21:
+            print("Past 21! You busted...")
+            PlayerTurn = False
+            match_going = False
 
 
-	elif player_input == "stand":
-		PlayerTurn = False
-		print("**Dealer's Turn**")
-		deck.draw_card(Dealer_Hand,"Dealer")
-		dealer_score = deck.ScoreCard(Dealer_Hand)
-		# Dealer must hit until they beat 16
-		while dealer_score <= 16:
-			deck.draw_card(Dealer_Hand,"Dealer")
-			dealer_score = deck.ScoreCard(Dealer_Hand)
-			
-			if dealer_score >= player_score and dealer_score <= 21:
-				print("Dealer wins!")
-				match_going = endMatch()
-			elif dealer_score < player_score and player_score <= 21:
-				print("You win!")
-				match_going = endMatch()
-			elif dealer_score > 21:
-				print("Dealer bust you win!")
-				match_going = endMatch()
+    elif player_input == "stand":
+        PlayerTurn = False
+        print("**Dealer's Turn**")
+        deck.draw_card(Dealer_Hand,"Dealer")
+        dealer_score = deck.ScoreCard(Dealer_Hand)
+        # Dealer must hit until they beat 16
+        while dealer_score <= 16:
+            deck.draw_card(Dealer_Hand,"Dealer")
+            dealer_score = deck.ScoreCard(Dealer_Hand)
+            
+            if dealer_score >= player_score and dealer_score <= 21:
+                print("Dealer wins!")
+                match_going = endMatch()
+            elif dealer_score < player_score and player_score <= 21:
+                print("You win!")
+                match_going = endMatch()
+            elif dealer_score > 21:
+                print("Dealer bust you win!")
+                match_going = endMatch()
 
 
 if dealer_score == player_score:
@@ -97,12 +94,12 @@ if dealer_score == player_score:
 
 user_continue_match = str(input("Continue Playing? Y/N").upper())
 if user_continue_match == "Y":
-	reset()
-	startMatch()
-	PlayerTurn = True
-	player_score = deck.ScoreCard(Player_Hand)
-	dealer_score = deck.ScoreCard(Dealer_Hand) 
-	user_continue_match = ""
-
+    reset()
+    match_going = startMatch()
+    PlayerTurn = True
+    player_score = deck.ScoreCard(Player_Hand)
+    dealer_score = deck.ScoreCard(Dealer_Hand) 
+    user_continue_match = ""
+    player_input = ""
 if user_continue_match == "N":
-	print("Your Are Safe To Close Program")
+    print("Your Are Safe To Close Program")
